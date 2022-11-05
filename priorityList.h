74c330e0a9a361807f749f0fffb3503ba7df32e0
DejanVukadinovic <dejanforbusiness@gmail.com>
2022-11-05 22:50:54 +0100

    PJ1 Lab1 - Separated

---
+++ priorityList.h
@@ -0,0 +1,45 @@
+#ifndef PJ1_PRIORITYLIST_H
+#define PJ1_PRIORITYLIST_H
+#endif //PJ1_PRIORITYLIST_H
+
+
+class Node {
+private:
+    int value;
+    Node* prev = nullptr;
+    Node* next = nullptr;
+public:
+    Node* getPrev(){
+        return this->prev;
+    };
+    Node* getNext(){
+        return this->next;
+    };
+    void setPrev(Node* val){
+        this->prev = val;
+    };
+    void setNext(Node* val){
+        this->next = val;
+    };
+    int getValue(){
+        return this->value;
+    };
+    void setValue(int val){
+        this->value = val;
+    };
+    Node();
+    Node(int val);
+    Node(int val, Node* previous);
+
+};
+
+class PriorityQueue {
+private:
+    Node* head = nullptr;
+    Node* tail = nullptr;
+public:
+    Node* enqueue(int val);
+    int dequeue();
+    bool removeMember(int* val);
+    void writeAll();
+};
